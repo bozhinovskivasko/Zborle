3ef68793fb9f32055c2ee6488687083e998cbffd
Vasko Bozinovski <vbozinovski16@gmail.com>
2022-06-30 23:43:14 +0200

    added minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,85 +7,142 @@ import { WORDS } from './words';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  ngOnInit(): void {
-    this.todaysWord = this.words[Math.floor(Math.random() * this.words.length)];
-    this.novZbor();
-  }
+
+export class AppComponent implements OnInit {
+  ngOnInit(): void { }
+
+  //if the word is in the dictionary
   doesExist = false;
   words = WORDS;
+  todaysWord = this.words[Math.floor(Math.random() * this.words.length)];
+  row = 0;
+  //Entered word
+  word = '';
+  showErrorMessage = false;
 
-  todaysWord='шеќер';
-  counter: number = 0;
-  order: number = 0;
-  word: string = '';
-
-  novZbor() {
-    setTimeout (() => {
-      this.todaysWord = this.words[Math.floor(Math.random() * this.words.length)];
-   }, 86400000);
-  }
-
-  dodadi(char: any) {
-    console.log(this.todaysWord);
-    if(char.data === null) {
-      this.word = this.word.slice(0, this.word.length-1);
-      if(this.order !== 0) {
-        this.order--;
-      }
-    } else {
+  //function to add letters from input to the variable word
+  addLetter(char: any) {
+    //this is for deleting
+    if (char.data === null) {
+      this.word = this.word.slice(0, this.word.length - 1);
+      this.showErrorMessage = false;
+    } 
+    else { //this is for adding word
       this.word += char.data;
-      if(this.order !== 4){
-        this.order++;
-      }
-      
     }
-    if(this.word.length === 5) {
-      this.proveri();
+
+    if (this.word.length === 5) {
+      this.checkWord();
     }
   }
 
-  proveri() {
+  //function for checking if the word is valid and if it is for checking the letters
+  checkWord() {
+    this.findIfExists();
+    if (this.doesExist) { //check if the word exist in the database
+
+      //the word exists 
+      let element = document.getElementById(this.row + '');
+      element!.innerHTML = '';
+
+      //check if the word is todays word
+      if (this.word === this.todaysWord) {
+        //if it is color, paint the fields green
+        for (let i = 0; i < this.word.length; i++) {
+          this.color(element, this.word.charAt(i), "#008000");
+        }
+      }
+      else { //it is not so check the letters
+        this.checkLetters(element);
+        //after that reset the fields
+        this.row++;
+        this.word = '';
+        this.doesExist = false;
+      }
+    }
+    else { //it does not exist, so show message and disable next letter space
+      this.word = this.word.slice(0, this.word.length - 1);
+      this.showErrorMessage = true;
+    }
+  }
+
+  //function for searching the database for word
+  findIfExists() {
     console.log(this.todaysWord);
     this.word = this.word.toLowerCase();
-      for(let i=0; i<this.words.length; i++) {
-        if(this.word === this.words[i]) {
-          this.doesExist = true;
-          break;
-        }
+    for (let i = 0; i < this.words.length; i++) {
+      if (this.word === this.words[i]) {
+        this.doesExist = true;
+        break;
       }
-      if(this.doesExist){
-        if(this.word === this.todaysWord) {
-          let element = document.getElementById(this.counter + '');
-          element!.innerHTML='';
-          for(let i=0; i<this.word.length; i++) {
-            element!.innerHTML+='<input disabled type="text" placeholder="'+ this.word.charAt(i) +'" style="width: 70px; height: 70px; text-align: center; font-size: larger; font-weight: bold; margin: 4px; background-color: #90EE90;">';
-          }
-          alert("Честиткиииииии :)");
-        } else {
-          let element = document.getElementById(this.counter + '');
-          element!.innerHTML='';
-          for(let i=0; i<this.word.length; i++) {
-            if(this.word.charAt(i) === this.todaysWord.charAt(i)) {
-              element!.innerHTML+='<input disabled type="text" placeholder="'+ this.word.charAt(i) +'" style="width: 70px; height: 70px; text-align: center; font-size: larger; font-weight: bold; margin: 4px; background-color: #90EE90;">';
-            }
-            else if(this.todaysWord.includes(this.word.charAt(i))) {
-              element!.innerHTML+='<input disabled type="text" placeholder="'+ this.word.charAt(i) +'" style="width: 70px; height: 70px; text-align: center; font-size: larger; font-weight: bold; margin: 4px; background-color: yellow;">';
-            } 
-            else {
-              element!.innerHTML+='<input disabled type="text" placeholder="'+ this.word.charAt(i) +'" style="width: 70px; height: 70px; text-align: center; font-size: larger; font-weight: bold; margin: 4px; background-color: silver;">';
-            }
-          }
-          this.order = 0;
-          this.counter++;
-          this.word='';
-        }
+    }
+  }
 
-        if(this.counter === 5) {
-          alert("Не го погодивте зборот. Зборот што го баравме беше: " + this.todaysWord);
+  //function for cheking the letters and painting them the appropriate color
+  checkLetters(element: HTMLElement | null) {
+    //variable so I can keep track of the readed letters
+    let duplicates = '';
+
+    for (let i = 0; i < this.word.length; i++) {
+
+      //check if the letter is duplicate
+      if(duplicates.includes(this.word.charAt(i))) {
+        
+        //check if todays word has same letter twice also 
+        if(this.containsTwoLetters(this.todaysWord, this.word.charAt(i))) {
+          
+            //check if duplicate letter is in right spot
+            if(this.letterInRightSpot(i)) {
+
+              //the letter is in right place so add green color
+              this.color(element, this.word.charAt(i), "#008000");
+              duplicates += this.word.charAt(i);
+            }
+            else {
+              //the letter is not in right place so add yellow color
+              this.color(element, this.word.charAt(i), "yellow");
+              duplicates += this.word.charAt(i);
+            }
         }
-      }else{
-        alert("Непостоечки збор");
+        else {
+          //there are not two same letters in todays word so paint it silver
+          this.color(element, this.word.charAt(i), "silver");
+        }
+      } 
+
+      //check if the letter is not part of todays word
+      else if(!this.todaysWord.includes(this.word.charAt(i))) {
+        this.color(element, this.word.charAt(i), "silver");
       }
+
+      //check if letter is in right spot so paint it green
+      else if(this.letterInRightSpot(i)) {
+        this.color(element, this.word.charAt(i), "#008000");
+        duplicates += this.word.charAt(i);
+      }
+      else {
+        //the letter is in todays word but, not in right place so paint it yellow
+        this.color(element, this.word.charAt(i), "yellow");
+        duplicates += this.word.charAt(i);
+      }
+    }
+  }
+
+  //function for cheking if letter is in right place
+  letterInRightSpot(i: number): boolean {
+    if(this.word.charAt(i) === this.todaysWord.charAt(i)) {
+      return true;
+    }
+    return false;
+  }
+
+  //function for cheking if there are two same letters
+  containsTwoLetters(str: string, char: string) {
+    return str.indexOf(char) != str.lastIndexOf(char);
+  }
+
+  //function for coloring the input field
+  color(element: HTMLElement | null, char: string, color: string) {
+    element!.innerHTML += '<input disabled type="text" placeholder="' + char + '" style="width: 70px; height: 70px; text-align: center; font-size: larger; font-weight: bold; margin: 4px; border-radius: 7%; background-color:' + color + ';">';
   }
 }
